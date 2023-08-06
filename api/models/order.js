79f49683fea8a1d89e7cac6dca99af956ecc2cfa
Anshuman <anshumansp16@gmail.com>
2023-08-06 19:03:52 +0530

    Updated Orders queries and added NPM start to Package JSON

--- api/models/order.js
+++ api/models/order.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
+// Creating the Order Schema for defining the Query Keys and their Data Types to be store
 const orderSchema = mongoose.Schema ({
     _id: mongoose.Schema.Types.ObjectId,
-    productId : String,
-    quantity: Number
-});
+    //Product is having the object id of the existing produt and it is required 
+    product: { type: mongoose.Schema.Types.ObjectId, ref:'Product', required: true },
+    // Quantity should be of Number and if no quantity is placed it will set to 1 by default
+    quantity: { type: Number, default: 1},
+  });
 
+  // Making the Model and new Collection of products and using the Product Schema
 module.exports = mongoose.model('Order', orderSchema);
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,53 +1,150 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../models/order');
-const mongoose = require('mongoose');
+const Product = require("../models/product");
+const Order = require("../models/order");
+const mongoose = require("mongoose");
 
-router.get('/', (req, res, next)=> {
-    res.status(200).json({
-        message: 'Orders have been fetched'
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then((docs) => {
+      res.status(201).json({
+        count: docs.length,
+        orders: docs.map((doc) => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              description: "FIND MORE ABOUT IT",
+              url: "http://localhost:3000/orders/" + doc._id,
+            },
+          };
+        }),
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
-router.post('/', (req, res, next)=> {
-    const order = {
-      productId: req.body.productId,
-      quantity: req.body.quantity,
-    };
-
-    
-    res.status(201).json({
-        message: 'Orders have been created',
-        createdOrder: order
+router.post("/", (req, res, next) => {
+  Product.findById(req.body.product)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({
+          message: "Product Not Found",
+        });
+      }
+      const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        product: req.body.product,
+        quantity: req.body.quantity,
+      });
+      return order.save();
     })
-})
-
-router.get('/:orderId', (req, res, next)=> {
-    res.status(200).json({
-        message: 'Order Details', 
-        orderId: req.params.orderId
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Order Created Successfully",
+        createdProduct: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity,
+        },
+        request: {
+          type: "GET",
+          description: "FIND MORE ABOUT IT",
+          url: "http://localhost:3000/orders/" + result._id,
+        },
+      });
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
-router.post('/:orderId', (req, res, next)=> {
-    const orderId = req.params.orderId
-    res.status(200).json({
-        message: `Order with OrderId ${orderId} Created`, 
+router.get("/:orderId", (req, res, next) => {
+  Order.findById(req.params.orderId)
+    .select("_id product quantity")
+    .exec()
+    .then((order) => {
+      if(!order) {
+        res.status(404).json({
+            message: 'Order Not Found'
+        })
+      }
+      res.status(201).json({
+        order: order,
+        request: {
+          type: "GET",
+          description: "FIND ALL ORDERS",
+          url: "http://localhost:3000/orders/",
+        },
+      });
     })
-})
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
-router.patch('/:orderId', (req, res, next)=> {
-    const orderId = req.params.orderId
-    res.status(200).json({
-        message: `Order ${orderId} Updated`, 
+router.patch("/:orderId", (req, res, next) => {
+  const orderId = req.params.orderId;
+  const newQuantity = req.body.newQuantity;
+  const updateOps = {
+    quantity: newQuantity,
+  };
+  Order.findByIdAndUpdate({ _id: orderId }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Order Updated Successfully",
+        request: {
+          type: "GET",
+          description: "FIND MORE DETAIL OF THIS ORDER",
+          url: "http://localhost:3000/orders/" + result._id,
+        },
+      });
     })
-})
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 router.delete('/:orderId', (req, res, next)=> {
     const orderId = req.params.orderId;
-    res.status(200).json({
-        message: `Order ${orderId} Deleted`, 
+    Order.findByIdAndRemove({_id: orderId})
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Order Deleted Successfully",
+        request: {
+          type: "POST",
+          description: "CREATE A NEW ORDER",
+          url: "http://localhost:3000/orders/",
+          body: {
+            productId : 'ID',
+            quantity :  'Number'
+          }
+        },
+      });
     })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 })
 
 module.exports = router;
